4122c9754917ce1568b3c30112c8623a4075fa5f
AlbinCond <condettealbin@gmail.com>
2025-03-05 13:42:58 +0100

    ajout login au user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,92 +1,99 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import prisma from '../client';
-import { user } from './user.router';
 import { Prisma } from '@prisma/client';
 import JWT from 'jsonwebtoken';
 
 export const creerUser = async (req: Request, res: Response) => {
-  const { email,password } = req.body;
+  const { email, password } = req.body;
   try {
     const emailExist = await prisma.user.findUnique({
       where: {
         email: email
       }
     });
+
     if (emailExist) {
       res.status(400).send('Email déjà utilisé');
     } 
-    
     else if (email === undefined || password === undefined) {
       const missingFields = [];
       if (email === undefined) missingFields.push('email');
       if (password === undefined) missingFields.push('password');
       res.status(400).send(`Veuillez remplir tous les champs: ${missingFields.join(', ')}`);
+    } 
+    else {
+      const user = await prisma.user.create({
+        data: {
+          email: email,
+          password: await bcrypt.hash(password, 10),
+        },
+      });
+      res.status(201).send(`User ${user.id} créé`);
     }
+  } 
 
-    else
-    {const user = await prisma.user.create({
-      data: {
-        email: email,
-        password: await bcrypt.hash(password, 10),
-      },
-    });
-    res.status(201).send(`User ${user.id} créé`);}
-  } catch (error) {
+  catch (error) {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      res.status(500).json({message: 'Une erreur' + `$(error.code) / $(erreur.message)`});
+      res.status(500).json({ message: 'Une erreur ' + error.code + ' / ' + error.message });
     }
 
-    if(error instanceof Prisma.PrismaClientUnknownRequestError){
+    if (error instanceof Prisma.PrismaClientUnknownRequestError) {
       res.status(500).send(`Une erreur est survenue : ${error.message}`);
     }
     res.status(500).send(`Erreur serveur : ${error}`);
   }
 }
 
-export const connectUser = async (req: Request, res: Response) => {
+export const connectUser = async (req: Request|any, res: Response|any) => {
   const { email, password } = req.body;
   try {
-
-    const emailExist = await prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
       where: {
         email: email
       }
     });
-    if (!emailExist) {
-      res.status(400).send('Le compte n\'existe pas');
+
+    if (!user) {
+      res.status(404).send('Le compte n\'existe pas');
     } 
+    
+    else {
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect) {
+        return res.status(400).send('Invalid email or password');
+      } 
+      
+      else {
+        const mysecretkey = process.env.JWT_SECRET;
+        const expire = process.env.JWT_EXPIRE;
 
-    if (!isPasswordCorrect) {
-      return res.status(401).send('Invalid email or password');
-    }
+        if (!mysecretkey) {
+          throw new Error('JWT_SECRET is not defined');
+        }
 
-    else{
-      const mysecretkey = process.env.JWT_SECRET;
-      const expire = process.env.JWT_EXPIRE;
+        const payload = {
+          email: email,
+        };
 
-      if (!mysecretkey) {
-        throw new Error('JWT_SECRET is not defined');
+        const token = JWT.sign(payload, mysecretkey, { expiresIn: expire });
+
+        res.status(201).json({
+          msg: "User connecté",
+          token: token
+        });
       }
-
-      const payload = {
-        email: email,
-        password: password,
-      };
-    
-      const token = JWT.sign(payload, mysecretkey, { expiresIn: expire });
-    
-      res.status(200).json({
-        msg: "User connecté",
-        token: token
-      });};
-
+    }
   } catch (error) {
     res.status(400).send(`Erreur serveur : ${error}`);
   }
-
-
-
 }
+
+export const afficheUser = async (_req: Request, res: Response) => {
+  try {
+    res.status(200).json(await prisma.user.findMany());
+  } catch (error) {
+    res.status(400).send(`Erreur serveur : ${error}`);
+  }
+};
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { creerUser, connectUser } from './user.controller';
-
+import { creerUser, connectUser, afficheUser } from './user.controller';
 
 export const user = Router();
 
 user.post('/users', creerUser);
 user.post('/users/login', connectUser);
+user.get('/users/affiche', afficheUser);
